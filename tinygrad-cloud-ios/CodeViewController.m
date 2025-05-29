00a3265c32895bda059262c542c8e86cab0fbf73
roryclear <roryclear.rc@gmail.com>
2025-05-29 11:42:00 +0100

    remove editing func

--- tinygrad-cloud-ios/CodeViewController.m
+++ tinygrad-cloud-ios/CodeViewController.m
@@ -12,31 +12,21 @@
         self.title = title;
         self.view.backgroundColor = [UIColor systemBackgroundColor];
 
-        // Text view
         self.textView = [[UITextView alloc] initWithFrame:CGRectZero];
         self.textView.text = code;
         self.textView.font = [UIFont monospacedSystemFontOfSize:14 weight:UIFontWeightRegular];
-        self.textView.editable = YES;
+        self.textView.editable = NO;
         self.textView.translatesAutoresizingMaskIntoConstraints = NO;
-        
-        UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, UIScreen.mainScreen.bounds.size.width, 44)];
-        UIBarButtonItem *flexSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
-        UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(dismissKeyboard)];
-        toolbar.items = @[flexSpace, doneButton];
-        [self.textView setInputAccessoryView:toolbar];
 
-        // Copy button
         UIButton *copyButton = [UIButton buttonWithType:UIButtonTypeSystem];
         [copyButton setTitle:@"Copy" forState:UIControlStateNormal];
         copyButton.titleLabel.font = [UIFont boldSystemFontOfSize:16];
         [copyButton addTarget:self action:@selector(copyToClipboard) forControlEvents:UIControlEventTouchUpInside];
         copyButton.translatesAutoresizingMaskIntoConstraints = NO;
 
-        // Add views
         [self.view addSubview:self.textView];
         [self.view addSubview:copyButton];
 
-        // Constraints
         [NSLayoutConstraint activateConstraints:@[
             [self.textView.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor],
             [self.textView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:10],
@@ -48,53 +38,12 @@
             [copyButton.bottomAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.bottomAnchor constant:-20],
             [copyButton.heightAnchor constraintEqualToConstant:44],
         ]];
-
-        // Keyboard notifications
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(keyboardWillShow:)
-                                                     name:UIKeyboardWillShowNotification
-                                                   object:nil];
-
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(keyboardWillHide:)
-                                                     name:UIKeyboardWillHideNotification
-                                                   object:nil];
     }
     return self;
-}
-
-- (void)dismissKeyboard {
-    [self.textView resignFirstResponder];
 }
 
 - (void)copyToClipboard {
     [UIPasteboard generalPasteboard].string = self.textView.text;
 }
 
-- (void)keyboardWillShow:(NSNotification *)notification {
-    CGRect kbFrame = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
-    double duration = [notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
-
-    UIEdgeInsets insets = UIEdgeInsetsMake(0, 0, kbFrame.size.height + 10, 0);
-
-    [UIView animateWithDuration:duration animations:^{
-        self.textView.contentInset = insets;
-        self.textView.scrollIndicatorInsets = insets;
-    }];
-}
-
-- (void)keyboardWillHide:(NSNotification *)notification {
-    double duration = [notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
-
-    [UIView animateWithDuration:duration animations:^{
-        self.textView.contentInset = UIEdgeInsetsZero;
-        self.textView.scrollIndicatorInsets = UIEdgeInsetsZero;
-    }];
-}
-
-- (void)dealloc {
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
-}
-
 @end
-
